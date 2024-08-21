8a0f3eb6e7c0b86060a57c028df452e1848e1bbd
martinphan <cc891259@ens.uqam.ca>
2024-08-21 15:44:42 -0400

    Doc Postman ResourceServer

--- OAuth2ResourceServer/src/main/java/com/tutorial/oauth2resourceserver/config/SecurityConfig.java
+++ OAuth2ResourceServer/src/main/java/com/tutorial/oauth2resourceserver/config/SecurityConfig.java
@@ -11,6 +11,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	// Postman tests
+	// Generate a OAuth2 access token
+	// Configure
+	/*
+	*   Grant Type = "Authorization Code"
+	*   Callback URL = "https://oauthdebugger.com/debug"
+	*   Auth URL = "http://localhost:9000/oauth2/authorize"
+	*   Access Token URL = "http://localhost:9000/oauth2/token"
+	*   Client ID = "client"
+	*   Client Secret = "secret"
+	*   Scope = "read"
+	*   Client Authentication Method = "client_secret_basic"
+	*
+	* Then
+	*
+	*   Put access token in Authorization header with Bearer prefix
+	*   and send request to resource server with http://localhost:8080 once authenticated.
+	*   Before :401 Unauthorized / After :200 OK
+	* */
 
 	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
 	private String issuerUri;
